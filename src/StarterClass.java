e9fc2b0765cae8b7e43d5a929b02a1355e7cd0b3
Matt Porter <mp874510@ohio.edu>
2014-08-24 22:46:06 -0400

    Added Function to add a new Column to a table

--- src/StarterClass.java
+++ src/StarterClass.java
@@ -5,12 +5,9 @@ public class StarterClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		DataBaseConnection db = new DataBaseConnection();
-		try {
-			db.OpenDB("MyDB");
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		db.OpenDB("MyDB");
+		//db.AddTable("TABLE2");
+		db.AddColumn("TABLE2", "Name", "VARCHAR(25)");
 	}
 
 }
--- src/com/porter/dBase/DataBaseConnection.java
+++ src/com/porter/dBase/DataBaseConnection.java
@@ -3,16 +3,22 @@ package com.porter.dBase;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DataBaseConnection {
 	private String protocol = "jdbc:derby:";
 	private String EmbedDriver = "org.apache.derby.jdbc.EmbeddedDriver";
 	String dbName = "MyDB";
 	Connection conn;
-	public void OpenDB(String dbName) throws ClassNotFoundException
-	{
+	 Statement stmt = null;
+	public void OpenDB(String dbName) {
 		
-			Class.forName(EmbedDriver);
+			try {
+				Class.forName(EmbedDriver);
+			} catch (ClassNotFoundException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
 		
 		
 		try {
@@ -21,7 +27,48 @@ public class DataBaseConnection {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 	}
 	
+	public void AddTable(String TableName)
+	{
+		try {
+			stmt = conn.createStatement();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String sql = "CREATE TABLE "+TableName+"(ID INT)";
+		try {
+			stmt.execute(sql);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.print("Table already added");
+			//e.printStackTrace();
+		}
+	}
 	
+	public void AddColumn(String tableName, String ColName, String ColType)
+	{
+		try {
+			stmt = conn.createStatement();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		String sql = "ALTER TABLE " + tableName + " ADD " + ColName + " " + ColType;
+		
+		
+			try {
+				stmt.execute(sql);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		
+	}
 }
+	
+	
+
