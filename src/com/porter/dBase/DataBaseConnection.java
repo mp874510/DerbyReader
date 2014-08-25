0d595f9ac2f5e3efb84a643beee9a653b4e269ba
Matt Porter <mp874510@ohio.edu>
2014-08-24 21:56:46 -0400

    successfully connected to Derby Database with the DataBaseConnection Class

--- src/com/porter/dBase/DataBaseConnection.java
+++ src/com/porter/dBase/DataBaseConnection.java
@@ -1,8 +1,27 @@
 package com.porter.dBase;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DataBaseConnection {
-	public void OpenDB()
+	private String protocol = "jdbc:derby:";
+	private String EmbedDriver = "org.apache.derby.jdbc.EmbeddedDriver";
+	String dbName = "MyDB";
+	Connection conn;
+	public void OpenDB() throws ClassNotFoundException
 	{
 		
+			Class.forName(EmbedDriver);
+		
+		
+		try {
+			conn = DriverManager.getConnection(protocol + dbName + ";create=true");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
+	
+	
 }
